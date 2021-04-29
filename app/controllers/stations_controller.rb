dff5807123d50e95262cbddcf03177577dce235d
dbrandt1990 <dvbrandt90@gmail.com>
2021-04-28 21:28:19 -0600

    delete users stations, w/o deleting from db

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -7,14 +7,19 @@ class StationsController < ApplicationController
     end
 
     def add_user
+        #add check if user already has station
         station = Station.find(params[:id])
-        station.users << current_user
-        redirect_to "/users/#{current_user.id}/stations"
+        if current_user.stations.find_by(api_id: station.api_id).nil?
+            station.users << current_user
+            redirect_to "/users/#{current_user.id}/stations"
+        else
+            flash[:alert] = "You've already added that station."
+            redirect_to user_path(current_user)
+        end
     end
-#!changed to try and remove from users stations instead of Stations
+#!changed to try and remove from users stations instead of Stations but still deleteing station from db
     def destroy
-        station = current_user.stations.find_by(id: params[:id])
-        station.destroy
+        current_user.stations.delete(Station.find_by(id: params[:id]))
         redirect_to "/users/#{current_user.id}/stations"
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email, uniqueness:{message: "Email already exists."}
+    validates :email, uniqueness:{message: "already exists."}
     has_many :users_station
     has_many :stations, through: :users_station
     has_many :notes
